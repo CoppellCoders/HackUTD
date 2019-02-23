e5ff88e70fb455352dc3ad4b2aecde1f097f7d39
Rohith <rkarkala10@gmail.com>
2019-02-23 17:43:34 -0600

    profile

--- app/src/main/java/hackutd/com/dormdash/MainActivity.java
+++ app/src/main/java/hackutd/com/dormdash/MainActivity.java
@@ -4,13 +4,16 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.widget.FrameLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    FrameLayout content;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,17 +21,16 @@ public class MainActivity extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_profile:
-                    mTextMessage.setText("Profile");
-                    return true;
                 case R.id.navigation_events:
-                    mTextMessage.setText("Events");
+
                     return true;
+                case R.id.navigation_profile:
+                    changeFragment(new ProfileFragment());
                 case R.id.navigation_food:
-                    mTextMessage.setText("Food");
+
                     return true;
                 case R.id.navigation_orderHistory:
-                    mTextMessage.setText("Order History");
+
                     return true;
             }
             return false;
@@ -39,9 +41,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mTextMessage = (TextView) findViewById(R.id.message);
+        content = findViewById(R.id.content);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+    public boolean changeFragment(Fragment target){
+        FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
+        ft.replace(R.id.content, target);
+        ft.commit();
+        return true;
     }
 
 }
